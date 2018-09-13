34cb96c81d874b2e9aade1076e98329ce2cc2ec0
safal <safaldasg@gmail.com>
2018-09-13 18:42:28 +0530

    feat(add-user): add to localstorage worked

--- app/add-user/add-user.component.ts
+++ app/add-user/add-user.component.ts
@@ -1,22 +1,47 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import { registerElement } from 'nativescript-angular/element-registry';
 import { CardView } from 'nativescript-cardview';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+require('nativescript-localstorage');
+
 registerElement('CardView', () => CardView);
 
 @Component({
-    selector: "ns-add",
-    moduleId: module.id,
-    templateUrl: "./add-user.component.html",
+  moduleId: module.id,
+
+  selector: 'ns-add',
+  templateUrl: './add-user.component.html'
 })
 export class AddUserComponent implements OnInit {
-    first_name: string = '';
-    occupation: string = '';
-    constructor() { }
+  inputDataForm: FormGroup;
 
-    ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.inputDataForm = this.formBuilder.group({
+      first_name: ['', Validators.required],
+      occupation: ['', Validators.required]
+    });
+  }
+  ngOnInit(): void {}
+
+  submit() {
+    let currentUsersListInStorage = localStorage.getItem('users');
+    let usersList=[];
+    if (currentUsersListInStorage) {
+      usersList = JSON.parse(currentUsersListInStorage);
     }
-
-    submit() {
-
+    if (this.inputDataForm.valid) {
+      usersList.push(this.inputDataForm.value);
+      localStorage.setItem('users', JSON.stringify(usersList));
+      console.log(JSON.stringify(localStorage.getItem('users')));
+    }else{
+        this.validateForm('inputDataForm')
     }
+  }
+  viewAll() {}
+  validateForm(form:string) {
+    Object.keys(this[form].controls).forEach(field => {
+      const control = this[form].get(field);
+      control.markAsTouched({ onlySelf: true });
+    });
+  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,34 +1,27 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { AppRoutingModule } from "./app.routing";
-import { AppComponent } from "./app.component";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptFormsModule } from 'nativescript-angular/forms';
+import { NativeScriptHttpModule } from 'nativescript-angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
-
-import { NativeScriptFormsModule } from "nativescript-angular/forms";
-
-import { NativeScriptHttpModule } from "nativescript-angular/http";
-import { AddUserComponent } from "~/add-user/add-user.component";
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
-    imports: [
-        NativeScriptModule,
-        AppRoutingModule,
-        NativeScriptFormsModule,
-        NativeScriptHttpModule
-    ],
-    declarations: [
-        AppComponent, AddUserComponent
-    ],
-    providers: [
-    ],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  bootstrap: [AppComponent],
+  imports: [
+    NativeScriptModule,
+    AppRoutingModule,
+    NativeScriptFormsModule,
+    NativeScriptHttpModule,
+    ReactiveFormsModule
+  ],
+  declarations: [AppComponent, AddUserComponent],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 /*
 Pass your application module to the bootstrapModule function located in main.ts to start your app
 */
-export class AppModule { }
+export class AppModule {}
